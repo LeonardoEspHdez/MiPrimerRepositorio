f4b9cb846396d7662cad6bf8af7b86ade2f0d928
Leonardo Esparza Hernández <42401839@uaz.edu.mx>
2025-10-16 14:04:21 -0600

    Mi primer commit

--- src/miprimerrepositorio/MiPrimerRepositorio.java
+++ src/miprimerrepositorio/MiPrimerRepositorio.java
@@ -15,6 +15,6 @@ public class MiPrimerRepositorio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Mi primer repositorio en Git");
     }
-    
 }
